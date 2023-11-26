43c7092c9ab8cef340e2d104e30de496827fe717
AngeloChDev <124574223+AngeloChDev@users.noreply.github.com>
2023-11-26 06:34:00 +0100

    Update req.py

--- req.py
+++ req.py
@@ -1,9 +1,5 @@
 import requests
 url='https://angelochdev.github.io/website_test_dci/games.html'
-page = requests.get(url)
-re = page.request
-if re.method == 'POST':
-      with open('re.txt', 'w') as f:
-            f.write('hghggj')
-            f.close()
-      print('oi')
+def me(x):
+      t=[[i for i in x] for j in range(4)]
+      return t
